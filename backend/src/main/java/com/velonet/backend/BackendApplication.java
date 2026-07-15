78f5ae6906b8e51f36395b6f6a65ecf85f4a74d0
LeonelaRivarola <leonelarivarola6322@gmail.com>
2026-07-15 12:49:18 -0300

    chore: instalaciones de todas las independencias

--- backend/src/main/java/com/velonet/backend/BackendApplication.java
+++ backend/src/main/java/com/velonet/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.velonet.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.velonet.backend.dto.TestDto;
+// import com.velonet.backend.dto.TestDto;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -11,10 +11,10 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 
-		TestDto dto = new TestDto();
-		dto.setNombre("Juan");
+		// TestDto dto = new TestDto();
+		// dto.setNombre("Juan");
 
-		System.out.println(dto.getNombre());
+		// System.out.println(dto.getNombre());
 	}
 
 }
